d13a036194bad1970556d2ce37f2d4804402cf30
Must57 <62475384+Must57@users.noreply.github.com>
2023-01-07 19:22:04 +0100

    #3 Page Action Photo
    Page permettant de choisir de prendre une photo ou d'en choisir une.

--- src/components/ActionPage.js
+++ src/components/ActionPage.js
@@ -2,18 +2,38 @@ import React, { useState } from "react";
 import {
   View,
   StyleSheet,
+  Button,
+  Alert,
+  Image,
 } from "react-native";
 
 
 import Colors from "../definitions/Colors";
+import Assets from "../definitions/Assets";
 
 const ActionPage = ({ navigation }) => {
-
   return (
     <View style={styles.container}>
-      <View style={styles.actionPageContainer}>
-        
+      <Image
+        style={styles.illustration}
+        source={Assets.images.illustrationCam}
+      />
+      
+      <View style={styles.actionBottom}>
+        <Button
+          title="Prendre une photo"
+          color={Colors.babyBlue}
+          Style={styles.btnPrPhoto}
+          onPress={() => Alert.alert('Prendre une photo')}
+        />
+        <Button
+          title="Choisir une photo"
+          style={styles.btnChPhoto}
+          color={Colors.grayBlue}
+          onPress={() => Alert.alert('Choisir une photo')}
+        />
       </View>
+      
     </View>
   );
 };
@@ -21,15 +41,34 @@ const ActionPage = ({ navigation }) => {
 export default ActionPage;
 
 const styles = StyleSheet.create({
+  
   container: {
     flex: 1,
     paddingHorizontal: 12,
-    marginTop: 16,
+    alignItems: "center",
+    alignContent: "stretch",
+    width: "100%",
+    paddingLeft: 0,
+    paddingRight: 0,
   },
-  actionPageContainer: {
-    marginBottom: 16,
+  actionBottom: {
+    alignContent: "stretch",
+    width: "100%",
+    position: "absolute",
+    bottom: 15,
   },
-  inputSearchTerm: {
-    marginBottom: 16,
+  btnPrPhoto: {
+    flex: 1,
+    backgroundColor: Colors.babyBlue,
+  },
+  btnChPhoto: {
+    flex: 1,
+    backgroundColor: Colors.grayBlue,
+  },
+  illustration: {
+    marginTop: 150,
+    width: 150,
+    height: 200,
+    marginLeft: 35,
   },
 });
--- src/definitions/Colors.js
+++ src/definitions/Colors.js
@@ -1,5 +1,7 @@
 const Colors = {
     primary_blue: "#5686E1",
+    babyBlue: "#7DB9FF",
+    grayBlue: "#5686E1",
   };
   
 export default Colors;
