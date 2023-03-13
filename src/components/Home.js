018dff1bc15de2e9db4ce03b8ea6145740d0618f
Must57 <62475384+Must57@users.noreply.github.com>
2023-03-13 08:41:14 +0100

    fix home2

--- src/components/Home.js
+++ src/components/Home.js
@@ -55,18 +55,6 @@ const Home = ({ navigation }) => {
   if (savedColors === null) {
     return <Text style={styles.emptyText}>Aucune recherche effectué pour l'instant</Text>;
   }
-  
-  const images = [
-    {
-      image:'https://images.unsplash.com/photo-1567226475328-9d6baaf565cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
-      desc: 'Silent Waters in the mountains in midst of Himilayas',
-    },
-    {
-      image:'https://images.unsplash.com/photo-1455620611406-966ca6889d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1130&q=80',
-      desc:
-       'Red fort in India New Delhi is a magnificient masterpeiece of humans',
-    },
-  ]
  
   return (
     <View style={styles.container}>
